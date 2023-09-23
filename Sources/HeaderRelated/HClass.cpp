3a9717cb1c35dc5977dbb755014e76a66467d2e8
Motisma479 <robion.mathieu.16@gmail.com>
2023-09-24 00:30:48 +0200

    [modified]Constructor added to cpp

--- Sources/HeaderRelated/HClass.cpp
+++ Sources/HeaderRelated/HClass.cpp
@@ -1,1 +1,21 @@
 #include "HeaderRelated/HClass.hpp"
+
+HClass::HClass(std::string _name) : name(_name)
+{
+    
+}
+
+void HClass::CreateVariables()
+{
+
+}
+
+void HClass::CreateConsDesTructor()
+{
+
+}
+
+void HClass::CreateFunction()
+{
+
+}
